f4a6cca410ee16d64b9a249a5279e3089c5d13a9
eyob-teferi <eyobteferi444@gmail.com>
2024-09-04 13:36:26 -0700

    nothing

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import ApiClient from "../services/api-client";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
