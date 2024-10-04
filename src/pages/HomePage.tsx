3e1cf225ece3be3f15d2eabb445dd249bf313edf
eyob-teferi <eyobteferi444@gmail.com>
2024-10-03 21:34:44 -0700

    setup routing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,13 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+import { Grid, Show, GridItem, HStack } from '@chakra-ui/react'
+import GameGrid from '../components/GameGrid'
+import GameHeading from '../components/GameHeading'
+import GenreList from '../components/GenreList'
+import PlatformSelector from '../components/PlatformSelector'
+import SortSelector from '../components/SortSelector'
 
-
-
-function App() {
-  
+const HomePage = () => {
   return (
-    <>
-      <Grid
+    <Grid
         templateAreas={{
           base: `"main"`,
           lg: `"aside main"`,
@@ -37,8 +31,7 @@ function App() {
           <GameGrid />
         </GridItem>
       </Grid>
-    </>
-  );
+  )
 }
 
-export default App;
+export default HomePage
